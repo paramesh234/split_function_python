115548db96c27ffa34187cfa84c2e9b631eb65b5
paramesh <gone.paramesh@gmail.com>
2024-10-17 18:31:25 +0530

    first commit

---
+++ prime_number.py
@@ -0,0 +1,11 @@
+#prime number program
+num=11
+count=0
+for fa in range(1,num+1):
+    if num%fa==0:
+        count=count+1
+if count==2:
+    print('prime number')
+else:
+    print('not prime number')
+    
